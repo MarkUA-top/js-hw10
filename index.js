cd3c2d9c1f8f151e2ed67f409b56468fd7d80734
MarkUA <mark.d.nahorniak@gmail.com>
2024-12-07 19:46:05 +0200

    ашч

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log(randomNumber)
-const say = prompt("Вгадай цифру від 1 до 100")
+// // Task 1
+// const sayHello = () => {
+//   const conClick = confirm("Підтвердіть привітання")
+//   if (conClick === true){
+//     console.log("Hello World!")
+//   } else {
+//     alert("bruh confirm dude")
+//   }
+// }
+// sayHello()
+// // Task 2
+// const randomNumber = Math.floor(Math.random() * 100) + 1;
+// console.log(randomNumber);
+// const say = Number(prompt("Вгадай цифру від 1 до 100"));
 
-function resRanNum() {
-  const resNum = (guessField === randomNumber);
-  console.log(resNum)
+// function resRanNum() {
+//   if (say === randomNumber) {
+//     console.log("yes u did it");
+//   } else {
+//     console.log("bruh dude u can do it")
+//   }
+// }
 
-  return resNum
-}
+// resRanNum()
+// task 3
+// let res = 0;
+
+// function checkClick() {
+//   while (true) {
+//     const addClick = confirm("Click if u stupid");
+//     if (addClick) {
+//       res++;
+//       console.log(`U click ${res}`);
+//     } else {
+//       console.log("Bruh u dont click u stupid HAHAHA");
+//       break;
+//     }
+//   }
+// }
+
+// checkClick();
+
+// task 4
+
+// task 5
+const calculateDiscountedPrice = (price, discount, callback) => {
+  const discountedPrice = price - (price * discount / 100);
+  callback(discountedPrice);
+};
+
+const showDiscountedPrice = (discountedPrice) => {
+  console.log(`Discounted price: ${discountedPrice}`);
+};
+
+calculateDiscountedPrice(192, 73, showDiscountedPrice);
