5f9b7f108450732beb3e7ae734197952d2e8a783
MarkUA <mark.d.nahorniak@gmail.com>
2024-12-07 17:52:06 +0200

    f

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 console.log(randomNumber)
+const say = prompt("Вгадай цифру від 1 до 100")
 
-function checkGuess() {
+function resRanNum() {
   const resNum = (guessField === randomNumber);
   console.log(resNum)
 
   return resNum
 }
-
-checkGuess()
