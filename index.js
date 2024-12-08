92d3238ae8681bd84d5d4b7ac403b56be17adff8
MarkUA <mark.d.nahorniak@gmail.com>
2024-12-08 10:22:53 +0200

    js

--- index.js
+++ index.js
@@ -41,15 +41,37 @@
 // checkClick();
 
 // task 4
+// Напишіть функцію, яка повертає новий масив,
+// в якому всі елементи масиву помножені на задане число,
+// з використанням колбек-функції
+// const arr = [1, 2, 3, 4, 5];
+// function multiplyArray(arr, num, callback) {
+//     // код  
+// }
+// multiplyArray(arr, 3, () => {});
+
+// const arr = [1, 2, 3, 4, 5];
+
+// function multiplyArray(array, num, callback) {
+//     let res = [] 
+//     for (let i = 0; i < array.length; i++){
+//     res.push(array[i] * num)
+//     }
+//     callback(res)
+//     return res
+// }
+// const arrrrrrrrr = (rat) => alert(rat)
+
+// multiplyArray(arr, 3, arrrrrrrrr)
 
 // task 5
-const calculateDiscountedPrice = (price, discount, callback) => {
-  const discountedPrice = price - (price * discount / 100);
-  callback(discountedPrice);
-};
+// const calculateDiscountedPrice = (price, discount, callback) => {
+//   const discountedPrice = price - (price * discount / 100);
+//   callback(discountedPrice);
+// };
 
-const showDiscountedPrice = (discountedPrice) => {
-  console.log(`Discounted price: ${discountedPrice}`);
-};
+// const showDiscountedPrice = (discountedPrice) => {
+//   console.log(`Discounted price: ${discountedPrice}`);
+// };
 
-calculateDiscountedPrice(192, 73, showDiscountedPrice);
+// calculateDiscountedPrice(192, 73, showDiscountedPrice);
